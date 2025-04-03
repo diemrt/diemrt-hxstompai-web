1074d109f37ab0c7c7eb5f1066083579c789dcc0
diegomartignoni <156002111+diegomartignoni@users.noreply.github.com>
2025-04-04 00:24:02 +0200

    Add ThemeProvider component for theme management

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/ThemeProvider"
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
+    </ThemeProvider>
   )
 }
 
